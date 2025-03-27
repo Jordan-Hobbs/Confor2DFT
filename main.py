a0e3029dcd406c0cb8bf5a8a62dbcb1c3b2a5259
Jordan-Hobbs <jlhobbs0@gmail.com>
2025-03-27 19:31:14 +0000

    added conformer gen and sort functions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from confordft!")
+from molecules import RDMolGen
 
+def main():
+    mol = RDMolGen("O=C(OC1=CC(F)=C(C2=CC(F)=C(C(OC3=CC(F)=C(F)C(F)=C3)(F)F)C(F)=C2)C(F)=C1)C4=C(F)C=C(C5OCC(CCC)CO5)C=C4F")
+    mol.molecule_init()
+    mol.conformer_gen()
+    mol.find_min_confromer()
 
 if __name__ == "__main__":
     main()
--- molecules.py
+++ molecules.py
@@ -1,8 +1,8 @@
 from rdkit import Chem
-from rdkit.Chem import rdDistGeom
+from rdkit.Chem import rdDistGeom, rdForceFieldHelpers, Draw
 import py3Dmol
 
-class molec:
+class RDMolGen:
     def __init__(self, smiles):
         self.smiles = smiles
       
@@ -11,6 +11,27 @@ class molec:
         self.molecule_h = Chem.AddHs(self.molecule)
         Chem.rdCoordGen.AddCoords(self.molecule_h)
         rdDistGeom.EmbedMolecule(self.molecule_h)
+        rdForceFieldHelpers.MMFFOptimizeMolecule(self.molecule_h, maxIters=1000, ignoreInterfragInteractions=False)
+
+    def conformer_gen(self):
+        rdDistGeom.EmbedMultipleConfs(self.molecule_h, 10, params=rdDistGeom.ETKDGv3())
+        self.conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(self.molecule_h, maxIters=1000, ignoreInterfragInteractions=False)
+        if all(conf_set[0] == 0 for conf_set in self.conf_energy):
+            print("All conformers converged")
+        else:
+            print("Not all conformers converged")
+
+    def find_min_confromer(self):
+        min_energy_MMFF = 10000
+        for index, energy in enumerate(self.conf_energy):
+            if min_energy_MMFF > energy[1]:
+                min_energy_MMFF = energy[1]
+                min_energy_index_MMFF = index
+        self.mol_min = Chem.Mol(self.molecule_h, False, min_energy_index_MMFF)
+        Draw.MolToImage(self.mol_min) # This doesnt work?
+
+    def return_RD_molecule(self):
+        return self.molecule_h
 
     def threeD_view(self):
         view = py3Dmol.view(data=Chem.MolToMolBlock(self.molecule_h), style={"stick": {}, "sphere": {"scale": 0.3}})
