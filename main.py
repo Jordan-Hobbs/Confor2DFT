b9ca3b1b7f136d732cbd736af37094bf69f3167b
Jordan-Hobbs <jlhobbs0@gmail.com>
2025-04-01 20:11:34 +0100

    working on new version

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from molecules import RDMolGen
+import molecules
 
 def main():
-    mol = RDMolGen("O=C(OC1=CC(F)=C(C2=CC(F)=C(C(OC3=CC(F)=C(F)C(F)=C3)(F)F)C(F)=C2)C(F)=C1)C4=C(F)C=C(C5OCC(CCC)CO5)C=C4F")
-    mol.molecule_init()
-    mol.conformer_gen()
-    mol.find_min_confromer()
+
+    ## gen minimum conformer 
+    molecules.find_min_confromer("O=C(OC1=CC(F)=C(C2=CC(F)=C(C#N)C(F)=C2)C(F)=C1)C(C(F)=C3)=CC=C3CCC")
 
 if __name__ == "__main__":
     main()
