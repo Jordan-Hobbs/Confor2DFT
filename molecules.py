8fc792a70bad2ac010a65612e047d4083542cc1a
Jordan-Hobbs <jlhobbs0@gmail.com>
2025-04-01 20:08:31 +0100

    changed molecules from class to function

--- molecules.py
+++ molecules.py
@@ -1,42 +1,34 @@
 from rdkit import Chem
 from rdkit.Chem import rdDistGeom, rdForceFieldHelpers, Draw
-import py3Dmol
 
-class RDMolGen:
-    def __init__(self, smiles):
-        self.smiles = smiles
-      
-    def molecule_init(self):
-        self.molecule = Chem.MolFromSmiles(self.smiles)
-        self.molecule_h = Chem.AddHs(self.molecule)
-        Chem.rdCoordGen.AddCoords(self.molecule_h)
-        rdDistGeom.EmbedMolecule(self.molecule_h)
-        rdForceFieldHelpers.MMFFOptimizeMolecule(self.molecule_h, maxIters=1000, ignoreInterfragInteractions=False)
 
-    def conformer_gen(self):
-        rdDistGeom.EmbedMultipleConfs(self.molecule_h, 10, params=rdDistGeom.ETKDGv3())
-        self.conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(self.molecule_h, maxIters=1000, ignoreInterfragInteractions=False)
-        if all(conf_set[0] == 0 for conf_set in self.conf_energy):
-            print("All conformers converged")
-        else:
-            print("Not all conformers converged")
+def find_min_confromer(smiles, no_conf: int = 1, max_opt_iter: int = 1): 
 
-    def find_min_confromer(self):
-        min_energy_MMFF = 10000
-        for index, energy in enumerate(self.conf_energy):
-            if min_energy_MMFF > energy[1]:
-                min_energy_MMFF = energy[1]
-                min_energy_index_MMFF = index
-        self.mol_min = Chem.Mol(self.molecule_h, False, min_energy_index_MMFF)
-        Draw.MolToImage(self.mol_min) # This doesnt work?
+    ## Generate the minimimum structure of the a random conformer of input simles string
+    molecule = Chem.MolFromSmiles(smiles)
+    molecule_h = Chem.AddHs(molecule)
 
-    def return_RD_molecule(self):
-        return self.molecule_h
+    ## Generate conformers and optimise them
+    rdDistGeom.EmbedMultipleConfs(molecule_h, no_conf, params=rdDistGeom.ETKDGv3())
+    conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(molecule_h, max_opt_iter)
+    print(conf_energy)
+    ## Check convergence of optimisation
+    if all(conf_set[0] == 0 for conf_set in conf_energy):
+        print("All conformers converged")
+    else:
+        print("Not all conformers converged")
 
-    def threeD_view(self):
-        view = py3Dmol.view(data=Chem.MolToMolBlock(self.molecule_h), style={"stick": {}, "sphere": {"scale": 0.3}})
-        view.zoomTo()
-        return view
+    ## Find minimum energy conformer
+    min_energy_MMFF = 10000
+    for index, energy in enumerate(conf_energy):
+        if min_energy_MMFF > energy[1]:
+            min_energy_MMFF = energy[1]
+            min_energy_index_MMFF = index
+    mol_min = Chem.Mol(molecule_h, False, min_energy_index_MMFF)
+
+    Draw.MolToImage(mol_min)
+    return mol_min
+
     
 
     
