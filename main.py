4baa6af1c24efadbf0cee26c7338359647444a3e
Jordan Hobbs <j.l.hobbs@leeds.ac.uk>
2025-04-04 13:45:36 +0100

    now outputs crest input

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import molecules
+import crest_writer
 
 def main():
 
     ## gen minimum conformer 
-    molecules.find_min_confromer("O=C(OC1=CC(F)=C(C2=CC(F)=C(C#N)C(F)=C2)C(F)=C1)C(C(F)=C3)=CC=C3CCC")
+    molecule = molecules.find_min_confromer("O=C(OC1=CC(F)=C(C2=CC(F)=C(C(OC3=CC(F)=C(F)C(F)=C3)(F)F)C(F)=C2)C(F)=C1)C4=C(F)C=C(C5OCC(CCC)CO5)C=C4F", no_conf=10)
+
+    CREST = crest_writer.CRESTGen("test", molecule)
+    CREST.write_xyz()
+    CREST.write_toml("ancopt")
+    CREST.write_sh()
 
 if __name__ == "__main__":
     main()
