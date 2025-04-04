6029923adb4b32d6f7cc07e0add4a49d396e38e4
Jordan Hobbs <j.l.hobbs@leeds.ac.uk>
2025-04-04 13:44:56 +0100

    moved crest file writer out

--- molecules.py
+++ molecules.py
@@ -2,21 +2,24 @@ from rdkit import Chem
 from rdkit.Chem import rdDistGeom, rdForceFieldHelpers, Draw
 
 
-def find_min_confromer(smiles, no_conf: int = 1, max_opt_iter: int = 1): 
+def find_min_confromer(smiles, num_conf: int = 100, max_opt_iters: int = 1000): 
 
-    ## Generate the minimimum structure of the a random conformer of input simles string
+
+    ## Generate structure of a random unoptimised conformer from input smiles string
     molecule = Chem.MolFromSmiles(smiles)
     molecule_h = Chem.AddHs(molecule)
+    Chem.rdCoordGen.AddCoords(molecule_h)
+
 
     ## Generate conformers and optimise them
-    rdDistGeom.EmbedMultipleConfs(molecule_h, no_conf, params=rdDistGeom.ETKDGv3())
-    conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(molecule_h, max_opt_iter)
-    print(conf_energy)
+    rdDistGeom.EmbedMultipleConfs(molecule_h, num_conf, params=rdDistGeom.ETKDGv3())
+    conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(molecule_h, maxIters=max_opt_iters, ignoreInterfragInteractions=False)
     ## Check convergence of optimisation
     if all(conf_set[0] == 0 for conf_set in conf_energy):
         print("All conformers converged")
     else:
         print("Not all conformers converged")
+
 
     ## Find minimum energy conformer
     min_energy_MMFF = 10000
@@ -26,9 +29,4 @@ def find_min_confromer(smiles, no_conf: int = 1, max_opt_iter: int = 1):
             min_energy_index_MMFF = index
     mol_min = Chem.Mol(molecule_h, False, min_energy_index_MMFF)
 
-    Draw.MolToImage(mol_min)
     return mol_min
-
-    
-
-    
