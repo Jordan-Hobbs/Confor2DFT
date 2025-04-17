75a21ff14c5c8e308e52987513d8eef57e602b2b
Jordan Hobbs <j.l.hobbs@leeds.ac.uk>
2025-04-17 18:28:05 +0100

    calling file from CLI now properly interacts with the argparse structure. Files also now follow PEP8

--- crest_input.py
+++ crest_input.py
@@ -4,50 +4,60 @@ from rdkit.Chem import rdDistGeom, rdForceFieldHelpers
 import writers
 
 
-def write_crest_input(args):
+def crest_input(args):
+    # Generate minimum conformer
+    molecule = find_min_conformer(
+        args.SmilesString,
+        num_conf=args.NumberConformers,
+        max_opt_iters=args.MaximumIterations
+    )
 
-    ## Gen minimum conformer
-    molecule = find_min_confromer("O=C(OC1=CC(F)=C(C2=CC(F)=C(C(OC3=CC(F)=C(F)C(F)=C3)(F)F)C(F)=C2)C(F)=C1)C4=C(F)C=C(C5OCC(CCC)CO5)C=C4F", 
-                                num_conf=2, max_opt_iters=1000)
-    
-    ## Write CREST input files
-    writers.CRESTWriter("test", molecule)
+    # Write CREST input files
+    writers.CRESTWriter(
+        molecule,
+        args.FileName
+    )
 
 
-
-def find_min_confromer(smiles, num_conf: int = 100, max_opt_iters: int = 1000): 
-
-    #molecule = molecules.find_min_confromer("O=C(OC1=CC(F)=C(C2=CC(F)=C(C(OC3=CC(F)=C(F)C(F)=C3)(F)F)C(F)=C2)C(F)=C1)C4=C(F)C=C(C5OCC(CCC)CO5)C=C4F", 
-    #                            num_conf=2, max_opt_iters=1000)
-
-    print("----------------------------------------------------------------")
+def find_min_conformer(smiles, num_conf: int = 100, max_opt_iters: int = 1000):
+    print("\n----------------------------------------------------------------")
     print("Generating initial CREST input structure:\n")
 
-    ## Generate structure of a random unoptimised conformer from input smiles string
+    # Generate structure of a random unoptimized conformer from input SMILES 
+    # string
     molecule = Chem.MolFromSmiles(smiles)
     molecule_h = Chem.AddHs(molecule)
     Chem.rdCoordGen.AddCoords(molecule_h)
 
-    ## Generate conformers and optimise them
-    print(f"Generating {num_conf} conformers for initial sorting "
-          "and optimisation using RDKit")
-    rdDistGeom.EmbedMultipleConfs(molecule_h, num_conf, 
-        params = rdDistGeom.ETKDGv3())
-    conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(molecule_h, 
-        maxIters=max_opt_iters, ignoreInterfragInteractions=False)
-    ## Check convergence of optimisation
+    # Generate conformers and optimize them
+    print(
+        f"Generating {num_conf} conformers for initial sorting and "
+        "optimisation using RDKit."
+    )
+    rdDistGeom.EmbedMultipleConfs(
+        molecule_h,
+        num_conf,
+        params=rdDistGeom.ETKDGv3()
+    )
+    conf_energy = rdForceFieldHelpers.MMFFOptimizeMoleculeConfs(
+        molecule_h,
+        maxIters=max_opt_iters,
+        ignoreInterfragInteractions=False
+    )
+
+    # Check convergence of optimization
     if all(conf_set[0] == 0 for conf_set in conf_energy):
-        print("All conformers converged")
+        print("All conformers converged.")
     else:
-        print("WARNING! Not all conformers converged")
+        print("WARNING! Not all conformers converged.")
 
-    ## Find minimum energy conformer
-    min_energy_MMFF = 10000
-    for index, energy in enumerate(conf_energy):
-        if min_energy_MMFF > energy[1]:
-            min_energy_MMFF = energy[1]
-            min_energy_index_MMFF = index
-    mol_min = Chem.Mol(molecule_h, False, min_energy_index_MMFF)
+    # Find minimum energy conformer
+    min_energy = float("inf")
+    min_index = 0
+    for index, (_, energy) in enumerate(conf_energy):
+        if energy < min_energy:
+            min_energy = energy
+            min_index = index
+    mol_min = Chem.Mol(molecule_h, False, min_index)
 
-    print("")
     return mol_min
--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import cmd_parser
+import crest_input
 
 
 def main():
+    """Main function to run the program."""
     print("")
     print("   ___                 __               ___   ___    ___   _____ ")
     print("  / __|  ___   _ _    / _|  ___   _ _  |_  ) |   \\  | __| |_   _|")
-    print(" | (__  / _ \\ | ' \\  |  _| / _ \\ | '_|  / /  | |) | | _|    | |  ")
+    print(
+        " | (__  / _ \\ | ' \\  |  _| / _ \\ | '_|  / /  | |) | | _|    | |  "
+    )
     print("  \\___| \\___/ |_||_| |_|   \\___/ |_|   /___| |___/  |_|     |_| ")
-    print("Author: Dr Jordan Hobbs, University of Leeds")
+    print("Author: Dr. Jordan Hobbs, University of Leeds")
     print("https://github.com/Jordan-Hobbs/")
     print("")
 
-    ## Load program settings from cmd line and check for input config file
-    parser = cmd_parser.ProgramController()
+    # Load program settings from command line and check for input config file
+    commands = {
+        "crest_input": crest_input.crest_input
+    }
+
+    # Initialize parser and load config if provided
+    print("\n----------------------------------------------------------------")
+    print("Setting up config:\n")
+    parser = cmd_parser.ProgramController(commands)
     args = parser.get_args()
+
     if args.Config:
-        print("Config input detected. Attempting to load parameters from config file.")
+        print(
+            "Config input detected. Attempting to load parameters from "
+            "config file."
+        )
         parser.load_from_config()
+    else:
+        print(
+            "No config file provided. Using command line and defaults "
+            "instead."
+        )
 
-    
-
-
+    # Execute the function corresponding to the selected command
+    if hasattr(args, 'func'):
+        args.func(args)
+        print("")
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
